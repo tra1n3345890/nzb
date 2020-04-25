ced6360bbb13cbc709bb1de4f9c2dd97024f9879
Vladimir Mironov <v.mironov@viseven.com>
2020-04-25 22:33:15 +0300

    slick slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,22 @@
 $( () => {
 
-    console.log('Test');
+    $('#main-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+      });
+    $slick = $('#main-slider');
+    $next = $('#next-button');
+    $prev = $('#prev-button');
+
+    $next.on('click', () => {
+        $slick.slick('slickNext');
+    })
+
+    $prev.on('click', () => {
+        $slick.slick('slickPrev');
+    })
     
 });
